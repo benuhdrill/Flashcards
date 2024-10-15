09e117377ec94bba7521c3bb831aa98a40b8b2e9
benuhdrill <bgmach2023@fau.edu>
2024-10-15 16:22:37 -0400

    Finished Lab

---
+++ Flashcards/CreateFlashcardView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateFlashcardView.swift
+//  Flashcards
+//
+//  Created by Ben Gmach on 10/15/24.
+//
+
+import SwiftUI
+
+struct CreateFlashcardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateFlashcardView()
+}
