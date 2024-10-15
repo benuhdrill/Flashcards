b14efac64c104b5efccd1c75e00716b9db170b78
benuhdrill <bgmach2023@fau.edu>
2024-10-15 13:46:34 -0400

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Ben Gmach on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
